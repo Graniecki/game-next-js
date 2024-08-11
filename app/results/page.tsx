58a62c2ad32d0f8c2ef177b734c7853e00dc7c4b
Ihor Hranetskyi <i.hranetskyi@gmail.com>
2024-08-11 19:53:07 +0300

    deployment fix

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Image from 'next/image';
 import TitleMessage from './components/TitleMessage/TitleMessage';
 import Button from '../components/Button/Button';
@@ -19,7 +19,9 @@ function Results(): JSX.Element {
         <div className={styles.home__info}>
           <div>
             <h3 className={styles.home__subTitle}>Total score:</h3>
-            <TitleMessage />
+            <Suspense fallback={<div>Loading...</div>}>
+              <TitleMessage />
+            </Suspense>
           </div>
           <div className={styles.home__button}>
             <Button href="/game?new-attemtion" text="Try again" replace />
